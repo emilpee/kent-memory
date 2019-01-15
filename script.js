47a10f6235c571937b46a065aa2ef0331f329b9a
Åsa H <asa.hildingsson@hotmail.com>
2019-01-15 14:15:44 +0100

    kort inlagda

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 const allCards = [{
-        'name': 'kent1',
-        'img': 'kent1.jpg',
+        'name': 'kent',
+        'img': '/images/kent.jpg',
     },
     {
-        'name': 'kent2',
-        'img': 'kent2.jpg',
+        'name': 'verkligen',
+        'img': '/images/verkligen.jpg',
     },
-
+    {
+        'name': 'isola',
+        'img': '/images/isola.jpg',
+    },
+    {
+        'name': 'hagnesta-hill',
+        'img': '/images/hagnesta-hill.jpg',
+    },
+    {
+        'name': 'vapen-o-ammunition',
+        'img': '/images/vapen-o-ammunition.jpg',
+    },
+    {
+        'name': 'du-och-jag-doden',
+        'img': '/images/du-och-jag-doden.jpg',
+    },
+    {
+        'name': 'tillbaka-till-samtiden',
+        'img': '/images/tillbaka-till-samtiden.jpg',
+    },
+    {
+        'name': 'rod',
+        'img': '/images/rod.jpg',
+    },
+    {
+        'name': 'en-plats-i-solen',
+        'img': '/images/en-plats-i-solen.jpg',
+    },
+    {
+        'name': 'jag-ar-inte',
+        'img': '/images/jag-ar-inte.jpg',
+    },
+    {
+        'name': 'tigerdrottningen',
+        'img': 'tigerdrottningen.jpg',
+    },
+    {
+        'name': 'da-som-nu',
+        'img': '/images/da-som-nu.jpg',
+    },
 ];
