bc084e2d5d8403e51606d64dd308a2b6f4ae9e5e
Emil Petersson <emilpet93@gmail.com>
2019-01-16 15:51:54 +0100

    Created and displayed front of all memory cards

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
 // Alla kort
 const allCards = [{
-    'name': 'kent',
-    'img': '/images/kent.jpg',
+    name: 'kent',
+    img: 'images/kent.jpg',
 },
 {
-    'name': 'verkligen',
-    'img': '/images/verkligen.jpg',
+    name: 'verkligen',
+    img: 'images/verkligen.jpg',
 },
 {
-    'name': 'isola',
-    'img': '/images/isola.jpg',
+    name: 'isola',
+    img: 'images/isola.jpg',
 },
 {
-    'name': 'hagnesta-hill',
-    'img': '/images/hagnesta-hill.jpg',
+    name: 'hagnesta-hill',
+    img: 'images/hagnesta-hill.jpg',
 },
 {
-    'name': 'vapen-o-ammunition',
-    'img': '/images/vapen-o-ammunition.jpg',
+    name: 'vapen-o-ammunition',
+    img: 'images/vapen-o-ammunition.jpg',
 },
 {
-    'name': 'du-och-jag-doden',
-    'img': '/images/du-och-jag-doden.jpg',
+    name: 'du-och-jag-doden',
+    img: 'images/du-och-jag-doden.jpg',
 },
 {
-    'name': 'tillbaka-till-samtiden',
-    'img': '/images/tillbaka-till-samtiden.jpg',
+    name: 'tillbaka-till-samtiden',
+    img: 'images/tillbaka-till-samtiden.jpg',
 },
 {
-    'name': 'rod',
-    'img': '/images/rod.jpg',
+    name: 'rod',
+    img: 'images/rod.jpg',
 },
 {
-    'name': 'en-plats-i-solen',
-    'img': '/images/en-plats-i-solen.jpg',
+    name: 'en-plats-i-solen',
+    img: 'images/en-plats-i-solen.jpg',
 },
 {
-    'name': 'jag-ar-inte',
-    'img': '/images/jag-ar-inte.jpg',
+    name: 'jag-ar-inte',
+    img: 'images/jag-ar-inte.jpg',
 },
 {
-    'name': 'tigerdrottningen',
-    'img': 'tigerdrottningen.jpg',
+    name: 'tigerdrottningen',
+    img: 'images/tigerdrottningen.jpg',
 },
 {
-    'name': 'da-som-nu',
-    'img': '/images/da-som-nu.jpg',
+    name: 'da-som-nu',
+    img: 'images/da-som-nu.jpg',
 },
 ];
 
 // Spara spelplan i variabel och placera ut kortbehållare
 const board = document.getElementById('board');
-const cardContainer = document.createElement('section');
+const cardContainer = document.createElement('div');
 cardContainer.setAttribute('class', 'cardContainer');
 board.appendChild(cardContainer);
 
+
+// Skapa kopior av varje kort
+var allCardsCopy = allCards;
+const doubleImg = allCards.concat(allCardsCopy);
+
 // Skriv ut kortens framsidor med respektive bild och tilldela dem klass
-allCards.forEach(item => {
-    var card = document.createElement('div');
+doubleImg.forEach(item => {
+    var card = document.createElement('img');
     card.classList.add('card');
-    card.dataset.name = item.name;
-    card.style.backgroundImage = `url(${item.img})`;
+    card.dataset.value = item.name;
+    card.src = item.img;
+    cardContainer.appendChild(card);
 }) 
+
